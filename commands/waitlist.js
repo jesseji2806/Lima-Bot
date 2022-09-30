f46c0915f0a1407699bdee01ccd3e1e194d7fac0
deliberateEmbankment <55965022+deliberateEmbankment@users.noreply.github.com>
2022-09-29 23:47:08 -0400

    small visual changes

--- commands/waitlist.js
+++ commands/waitlist.js
@@ -8,27 +8,27 @@ module.exports = {
 
 	async execute(...args) {
 
-        const interaction = args[0];
+		const interaction = args[0];
         
-        const { cbId, day } = await cbSchema.findOne({ IGN: "AquariumStatus" });
+		const { cbId, day } = await cbSchema.findOne({ IGN: "AquariumStatus" });
 
-        if (day === 0 || day === 6) {
-            await interaction.reply({ content: "CB not currently active!", ephemeral: true });
-            return;
-        }
+		if (day === 0 || day === 6) {
+			await interaction.reply({ content: "CB not currently active!", ephemeral: true });
+			return;
+		}
 
-        let toReply = `Waitlist for day ${day} of CB${cbId}:\n`;
+		let toReply = `Waitlist for day ${day} of CB${cbId}:\n`;
 
-        for (let i = 1; i <= 5; ++i) {
-            const players = await cbSchema.find({ "cbId": cbId, "day": day, "IGN": { $ne: "AquariumStatus" }, "bossIds": i }, { "_id": 0, "IGN": 1 }, { sort: { "_id": 1 } });
-            if (players.length > 0) {
-                toReply += `**Boss ${i}:**\n`;
-                for (const player of players) {
-                    toReply += `${player.IGN}\n`;
-                }
-            }
-        }
+		for (let i = 1; i <= 5; ++i) {
+			const players = await cbSchema.find({ "cbId": cbId, "day": day, "IGN": { $ne: "AquariumStatus" }, "bossIds": i }, { "_id": 0, "IGN": 1 }, { sort: { "_id": 1 } });
+			if (players.length > 0) {
+				toReply += `**Boss ${i}:**\n`;
+				for (const player of players) {
+					toReply += `> ${player.IGN}\n`;
+				}
+			}
+		}
 
-        await interaction.reply(toReply);
-    },
+		await interaction.reply(toReply);
+	},
 }
