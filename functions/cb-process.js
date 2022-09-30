f1874e984d9f76dce593b6e2dd57d6177b1db6b2
deliberateEmbankment <55965022+deliberateEmbankment@users.noreply.github.com>
2022-09-30 00:23:31 -0400

    small joke

--- functions/cb-process.js
+++ functions/cb-process.js
@@ -186,6 +186,7 @@ async function setCollector(newCollector) {
 		} 
 		else if (i.customId === "ping-koishi") {
 			await i.editReply("Pinged the leader hihi");
+			await newCollector.coordination.send({ "content": `(CB${cbId}) ${i.user.tag} pinged leader <@386577302546546689>.` });
 			await newCollector.logs.send({ "content": `(CB${cbId}) ${i.user.tag} pinged leader <@386577302546546689>.` });
 		}
 	});
